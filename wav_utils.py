0d69ff4974038426ad0f594a37667c37f4c85dfd
Ricky <rich.ries@gmail.com>
2024-05-24 05:22:33 -0400

    using config defaults in wav_utils.py

--- wav_utils.py
+++ wav_utils.py
@@ -1,6 +1,12 @@
 import wave
+from config import CHANNELS, AUDIO_SAMPLE_RATE
 
-def save_wav(buffer, wav_file_path, channels=2, sample_width=2, frame_rate=44100):
+def save_wav(buffer,
+             wav_file_path,
+             channels=CHANNELS,
+             sample_width=2,
+             frame_rate=AUDIO_SAMPLE_RATE):
+
     wf = wave.open(wav_file_path, 'wb')
     wf.setnchannels(channels)
     wf.setsampwidth(sample_width)
