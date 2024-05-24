d4d1b3b6faf0d2c8e6c9bcfd0d1ec1f916d33f4f
Ricky <rich.ries@gmail.com>
2024-05-24 00:58:21 -0400

    server now runs in a thread, and can be shutdown by sentinel client

--- server.py
+++ server.py
@@ -9,23 +9,47 @@ class Server:
         self.host = host
         self.port = port
         self.chunk_size = chunk_size
+        self._running = threading.Event()
+        self.sentinel_message = b'x_end_x'
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)  # Listen for up to 5 connections
 
+    @property
+    def running(self):
+        return self._running.is_set()
+
     def serve(self):
         print(f"Server is listening on {self.host}:{self.port}")
-        while True:
+        self._running.set()
+        threading.Thread(target=self._serve).start()
+
+    def shutdown(self):
+        self._running.clear()
+        sentinel_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sentinel_socket.connect(('localhost', self.port))
+            sentinel_socket.sendall(self.sentinel_message)
+        except Exception as e:
+            print(f"Error sending shutdown sentinel: {e}")
+
+
+    def _serve(self):
+        while self.running:
             client_socket, client_address = self.server_socket.accept()
             print(f"Connection established with {client_address}")
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
             client_thread.start()
+        print('Server has been shut down.')
 
     def handle_client(self, client_socket):
         try:
             message = self.receive(client_socket)
             if message:
                 print(f"Received message of length: {len(message)} bytes")
+                if message == self.sentinel_message:
+                    print('Server got sentinel shutdown signal.')
+                    return client_socket.close()
                 response = self.process_message(message)
                 if response:
                     self.send(client_socket, response)
