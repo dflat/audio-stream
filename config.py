6818ed1f2a356cd6df6d45b2d18189669a0cbc50
Ricky <rich.ries@gmail.com>
2024-05-25 03:25:54 -0400

    added GPT token generation constants to config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ HOST = 'localhost'
 PORT = 5000
 CHUNK_SIZE = 8192
 
+# GPT token generation server
+OUTPUT_HOST = ''
+OUTPUT_PORT = 6000
+GPT_FINISH = 'x_end_x'
+
 # audio
 AUDIO_SAMPLE_RATE = 16000 # Sample rate that Whisper requires
 CHANNELS = 1
